c6c97c92c0506e75864071c92e764b8afdb6d936
Yusuke Kashiwagi <114566645+Waggy-Music@users.noreply.github.com>
2022-12-06 14:38:12 +0000

    Fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_12_071716) do
+ActiveRecord::Schema.define(version: 2022_10_17_031028) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "postimages", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
